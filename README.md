24978f09297e6ad0934038d12fb965df622ca7df
Paul Köhler <kontakt@paul-koehler.dev>
2022-03-30 17:55:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reliable_periodic_timer_example
+Dart Timer Periodic is not what you might think
---
+++ android/app/src/main/kotlin/com/example/reliable_periodic_timer_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reliable_periodic_timer_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
